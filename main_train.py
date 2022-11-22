49750d2aa7acbfde327b55cf222e8bbab460eb6d
NoTody <howardwong1780@gmail.com>
2022-11-22 04:18:54 -0500

    add LDM

---
+++ main_train.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import numpy as np
+import torchvision
+from torchvision import transforms
+import diffusers
+from diffusers import DDPMScheduler, DDIMScheduler, AutoencoderKL
+from diffusers.optimization import get_cosine_schedule_with_warmup
+import sys
+import utils
+import argparse
+from itertools import chain
+from dataclasses import dataclass
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--run_name", type=str, default="DDPM_CIFAR10_TEST")
+    parser.add_argument("--model_name", choices=["DDPM", "LDM"], type=str, default="DDPM")
+    parser.add_argument("--vae_model", choices=["CompVis/stable-diffusion-v1-4"], type=str, default="CompVis/stable-diffusion-v1-4")    
+    parser.add_argument("--num_train_timestamps", type=int, default=1000)
+    parser.add_argument("--dataset", choices=["CIFAR10", "FFHQ"], type=str, default="CIFAR10")
+    parser.add_argument("--image_size", type=int, default=32)
+    parser.add_argument("--train_batch_size", type=int, default=6)
+    parser.add_argument("--eval_batch_size", type=int, default=6)
+    parser.add_argument("--num_epochs", type=int, default=50)
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=1)
+    parser.add_argument("--learning_rate", type=float, default=1e-4)
+    parser.add_argument("--lr_warmup_steps", type=int, default=500)
+    parser.add_argument("--save_model_epochs", type=int, default=1)
+    parser.add_argument("--save_image_epochs", type=int, default=1)
+    parser.add_argument("--mixed_precision", type=str, default="fp16")
+    parser.add_argument("--output_dir", type=str, default="cifar10_saved")
+    parser.add_argument("--overwrite_output_dir", default=False, action="store_true")
+    parser.add_argument("--user_token", type=str, default="")
+    parser.add_argument("--seed", type=int, default=2022)
+    args = parser.parse_args()
+    return args  
+
+
+def get_dataloader(configs, image_transforms):
+    if config.dataset == "FFHQ":
+        # FFHQ dataset
+        dataset = utils.ffhq_Dataset("../dataset/ffhq/thumbnails128x128/", image_transforms)
+        config.image_size = 128
+    elif config.dataset == "CIFAR10":
+        #cifar10 dataset
+        dataset = torchvision.datasets.CIFAR10(root= "../dataset/", download=True, transform=image_transforms)
+        config.image_size = 32
+    elif config.dataset == "CIFAR100":
+        #cifar100 dataset
+        dataset = torchvision.datasets.CIFAR100(root= "../dataset/", download=True, transform=image_transforms)
+        config.image_size = 32
+    else:
+        raise ValueError("Invalid Dataset supplied")
+
+    train_loader = DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    return train_loader
+
+
+if __name__ == "__main__":
+    config = get_args()
+    #transforms
+    image_transforms = transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5]),
+        ]
+    )
+    # get trainloader
+    train_loader =  get_dataloader(config, image_transforms)
+
+    models = dict()
+    # get models, optimizer and noise scheduler
+    if config.model_name == "DDPM":
+        backbone_model = utils.get_default_unet(config)
+        models["backbone"] = backbone_model
+        model_params = backbone_model.parameters()
+        optimizer = torch.optim.AdamW(model_params, lr=config.learning_rate)
+        noise_scheduler = DDPMScheduler(num_train_timesteps=config.num_train_timestamps) 
+    elif config.model_name == "LDM":
+        backbone_model = utils.get_unconditional_unet(config)
+        #vae_model = AutoencoderKL.from_pretrained(config.vae_model, subfolder="vae", use_auth_token=config.user_token) 
+        vae_model = utils.get_custom_vae(config)
+        models["backbone"] = backbone_model
+        models["vae"] = vae_model
+        model_params = chain(backbone_model.parameters(), vae_model.parameters())
+        optimizer = torch.optim.AdamW(model_params, lr=config.learning_rate)
+        noise_scheduler = DDIMScheduler(num_train_timesteps=config.num_train_timestamps)
+    else:
+        raise NotImplementedError("Model Name not Implemented!")
+
+    # get learning rate scheduler
+    lr_scheduler = get_cosine_schedule_with_warmup(
+        optimizer=optimizer,
+        num_warmup_steps=config.lr_warmup_steps,
+        num_training_steps=(len(train_loader) * config.num_epochs),
+    )
+
+    # train model
+    utils.train_loop(config, models, noise_scheduler, optimizer, train_loader, lr_scheduler)
+
+
--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn.functional as F
+from itertools import chain
 import numpy as np
-from diffusers import UNet2DModel
+from diffusers import AutoencoderKL, UNet2DModel, UNet2DConditionModel
 from PIL import Image
 from torch.utils.data import Dataset
 import os
 import torchvision
 import diffusers
-from diffusers import DDPMPipeline
+from diffusers import DDPMPipeline, LDMPipeline
 import math
 from accelerate import Accelerator
 from tqdm.auto import tqdm
@@ -34,28 +35,54 @@ class ffhq_Dataset(Dataset):
 
 def get_default_unet(config):
     model = UNet2DModel(
-    sample_size=config.image_size,  # the target image resolution
-    in_channels=3,  # the number of input channels, 3 for RGB images
-    out_channels=3,  # the number of output channels
-    layers_per_block=2,  # how many ResNet layers to use per UNet block
-    block_out_channels=(128, 128, 256, 256, 512, 512),  # the number of output channes for each UNet block
-    down_block_types=( 
-        "DownBlock2D",  # a regular ResNet downsampling block
-        "DownBlock2D", 
-        "DownBlock2D", 
-        "DownBlock2D", 
-        "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
-        "DownBlock2D",
-    ), 
-    up_block_types=(
-        "UpBlock2D",  # a regular ResNet upsampling block
-        "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
-        "UpBlock2D", 
-        "UpBlock2D", 
-        "UpBlock2D", 
-        "UpBlock2D"  
-      ),
+        sample_size=config.image_size,  # the target image resolution
+        in_channels=3,  # the number of input channels, 3 for RGB images
+        out_channels=3,  # the number of output channels
+        layers_per_block=2,  # how many ResNet layers to use per UNet block
+        block_out_channels=(128, 128, 256, 256, 512, 512),  # the number of output channes for each UNet block
+        down_block_types=( 
+            "DownBlock2D",  # a regular ResNet downsampling block
+            "DownBlock2D", 
+            "DownBlock2D", 
+            "DownBlock2D", 
+            "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+            "DownBlock2D",
+        ), 
+        up_block_types=(
+            "UpBlock2D",  # a regular ResNet upsampling block
+            "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+            "UpBlock2D", 
+            "UpBlock2D", 
+            "UpBlock2D", 
+            "UpBlock2D"  
+        ),
     )
+    return model
+
+def get_unconditional_unet(config):
+    model = UNet2DModel(
+        sample_size=config.image_size,  # the target image resolution
+        in_channels=4,  # the number of input channels, 3 for RGB images
+        out_channels=4,  # the number of output channels
+        layers_per_block=2,  # how many ResNet layers to use per UNet block
+        block_out_channels=(320, 640, 1280, 1280),  # the number of output channes for each UNet block
+        down_block_types=(
+            "AttnDownBlock2D",
+            "AttnDownBlock2D",
+            "AttnDownBlock2D",
+            "DownBlock2D",
+        ), 
+        up_block_types=(
+            "UpBlock2D", 
+            "AttnUpBlock2D", 
+            "AttnUpBlock2D", 
+            "AttnUpBlock2D",
+        ),
+    )
+    return model
+
+def get_custom_vae(config):
+    model = AutoencoderKL()
     return model
 
 def make_grid(images, rows, cols):
@@ -65,7 +92,7 @@ def make_grid(images, rows, cols):
         grid.paste(image, box=(i%cols*w, i//cols*h))
     return grid
 
-def ddpm_evaluate(config, epoch, pipeline):
+def evaluate(config, epoch, pipeline):
     # Sample some images from random noise (this is the backward diffusion process).
     # The default pipeline output type is `List[PIL.Image]`
     images = pipeline(
@@ -82,11 +109,12 @@ def ddpm_evaluate(config, epoch, pipeline):
     return image_grid
 
 # train
-def ddpm_train_loop(config : dataclass, model : torch.nn.Module, noise_scheduler : diffusers.DDPMScheduler, optimizer : torch.optim.Adam, train_dataloader, lr_scheduler):
+def train_loop(config, models, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
     # Initialize accelerator and tensorboard logging
     accelerator = Accelerator(
         mixed_precision=config.mixed_precision,
         gradient_accumulation_steps=config.gradient_accumulation_steps,
+        device_placement=True,
         log_with="wandb",
         logging_dir=os.path.join(config.output_dir, "logs")
     )
@@ -96,10 +124,21 @@ def ddpm_train_loop(config : dataclass, model : torch.nn.Module, noise_scheduler
     # Prepare everything
     # There is no specific order to remember, you just need to unpack the 
     # objects in the same order you gave them to the prepare method.
-    model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
-        model, optimizer, train_dataloader, lr_scheduler
-    )
-    
+    if config.model_name == "DDPM":
+        backbone_model = models["backbone"]
+        backbone_model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+            backbone_model, optimizer, train_dataloader, lr_scheduler
+        )
+    elif config.model_name == "LDM":
+        backbone_model = models["backbone"]
+        backbone_model, vae_model = models["backbone"], models["vae"]
+        backbone_model, vae_model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+            backbone_model, vae_model, optimizer, train_dataloader, lr_scheduler
+        )
+        #vae.to(accelerator.device)
+    else:
+        raise NotImplementedError("Model Name not Implemented!") 
+
     global_step = 0
 
     # Now you train the model
@@ -112,28 +151,51 @@ def ddpm_train_loop(config : dataclass, model : torch.nn.Module, noise_scheduler
                 clean_images = batch[0]
             else:
                 clean_images = batch
-            # Sample noise to add to the images
-            noise = torch.randn(clean_images.shape).to(clean_images.device)
+            
             bs = clean_images.shape[0]
 
             # Sample a random timestep for each image
             timesteps = torch.randint(0, noise_scheduler.num_train_timesteps, (bs,), device=clean_images.device).long()
 
-            # Add noise to the clean images according to the noise magnitude at each timestep
-            # (this is the forward diffusion process)
-            noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
-            
-            with accelerator.accumulate(model):
-                # Predict the noise residual
-                noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
-                loss = F.mse_loss(noise_pred, noise)
-                accelerator.backward(loss)
+            # forward
+            if config.model_name == "DDPM":
+                with accelerator.accumulate(backbone_model):
+                    # Sample noise to add to the images
+                    noise = torch.randn(clean_images.shape).to(clean_images.device)
+                    # Add noise to the clean images according to the noise magnitude at each timestep
+                    # (this is the forward diffusion process)
+                    noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+                    # Predict the noise residual
+                    noise_pred = backbone_model(noisy_images, timesteps, return_dict=False)[0]
+                    # Compute Loss
+                    loss = F.mse_loss(noise_pred, noise)
+                    #model_params = backbone_model.parameters()
+            elif config.model_name == "LDM":
+                with accelerator.accumulate(backbone_model) as _, accelerator.accumulate(vae_model) as _:
+                    # Convert images to latent space
+                    latents = vae_model.module.encode(clean_images).latent_dist
+                    latents = latents * 0.18215
+                    # Sample noise that we'll add to the latents
+                    noise = torch.randn(latents.shape).to(latents.device)
+                    # Add noise to the latents according to the noise magnitude at each timestep
+                    # (this is the forward diffusion process)
+                    noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
+                    # Predict the noise residual
+                    noise_pred = backbone_model(noisy_latents, timesteps, return_dict=False)
+                    # Compute Loss
+                    loss = F.mse_loss(noise_pred, noise, reduction="none").mean([1, 2, 3]).mean()
+                    model_params = chain(backbone_model.parameters(), vae_model.parameters())
+            else:
+                raise NotImplementedError("Model Name not Implemented!")
 
-                accelerator.clip_grad_norm_(model.parameters(), 1.0)
-                optimizer.step()
-                lr_scheduler.step()
-                optimizer.zero_grad()
-            
+            # backward
+            accelerator.backward(loss)
+            accelerator.clip_grad_norm_(model_params, 1.0)
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()                    
+
+            # logging
             progress_bar.update(1)
             logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "step": global_step}
             progress_bar.set_postfix(**logs)
@@ -142,11 +204,18 @@ def ddpm_train_loop(config : dataclass, model : torch.nn.Module, noise_scheduler
 
         # After each epoch you optionally sample some demo images with evaluate() and save the model
         if accelerator.is_main_process:
-            pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
+            if config.model_name == "DDPM":
+                pipeline = DDPMPipeline(unet=accelerator.unwrap_model(backbone_model), scheduler=noise_scheduler)
+            elif config.model_name == "LDM":
+                pipeline = LDMPipeline(vqvae=accelerator.unwrap_model(vae_model), unet=accelerator.unwrap_model(backbone_model), scheduler=noise_scheduler)
+            else:
+                raise NotImplementedError("Model Name not Implemented!")
 
             if (epoch + 1) % config.save_image_epochs == 0 or epoch == config.num_epochs - 1:
-                eval_generations = ddpm_evaluate(config, epoch, pipeline)
+                eval_generations = evaluate(config, epoch, pipeline)
                 accelerator.log({"Generations" : wandb.Image(eval_generations)}, step=epoch)
 
             if (epoch + 1) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1:
-                pipeline.save_pretrained(config.output_dir) 
+                pipeline.save_pretrained(config.output_dir)
+
+ 
